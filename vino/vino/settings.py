70c6988fd9e795962a063c02ff13182aa02a3506
MarianoChacon <marianofchacon@gmail.com>
2022-05-11 20:39:30 -0300

    Entrega1Chacon

--- vino/vino/settings.py
+++ vino/vino/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'vino.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['F:/Marian/Documents/Python/Curso_Coderhouse/27620-python/Practica_VSC/proyectoFinalPrimeraEntrega/primeraEntregaProyectoFinal/vino/appvino/Templates/appvino'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'F:/Marian/Documents/Python/Curso_Coderhouse/27620-python/Practica_VSC/proyectoFinalPrimeraEntrega/primeraEntregaProyectoFinal/vino/appvino/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
